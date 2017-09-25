8ba3bffb36712c2d06d67cc4816dc2dabcc9cbf5
薛焱 <xueyan@xueyandeMacBook-Pro.local>
2017-09-25 21:17:57 +0800

    addf

--- Example/FYNModule/FYNAppDelegate.m
+++ Example/FYNModule/FYNAppDelegate.m
@@ -10,8 +10,6 @@
 
 @implementation FYNAppDelegate
 
-//sjdfoihgjsjkgha;ldfka
-//asdfasdjfkasdf
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
